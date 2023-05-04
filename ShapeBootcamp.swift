3ad60e190f075ff4312be9f1534b5afb8c3eed30
jungguen89 <leral3@nate.com>
2023-05-05 03:26:49 +0900

    형상 추가 사각형 / 타원형 / 캡슐형

--- ShapeBootcamp.swift
+++ ShapeBootcamp.swift
@@ -25,11 +25,25 @@ struct ShapeBootcamp: View {
 //                            lineWidth: 10, lineCap: .butt,
 //                            dash: [10]))              // 외곽선 설정 (스타일 적용)
             
-            Circle()
-                .trim(from: 0.25, to: 1)   // 형상 잘라내기
-                .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .butt, dash: [30]))
-                // trim과 stroke를 적절히 사용하면 로딩화면 표시기처럼 쓸수 있다.
+//            Circle()
+//                .trim(from: 0.25, to: 1)   // 형상 잘라내기
+//                .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .butt, dash: [30]))
+//                // trim과 stroke를 적절히 사용하면 로딩화면 표시기처럼 쓸수 있다.
             
+            Capsule()
+                .frame(width: 100, height: 200, alignment: .center)
+            
+            RoundedRectangle(cornerRadius: 10)
+                .trim(from: 0, to: 0.2)
+                .frame(width: 100, height: 100, alignment: .center)
+            
+            Rectangle()
+                .trim(from: 0.1, to: 0.8)
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(.red)
+            
+            Ellipse()
+                .frame(width: 200, height: 150, alignment: .center)
         }
     }
 }
