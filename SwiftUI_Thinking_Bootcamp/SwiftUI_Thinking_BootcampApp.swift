eee9a429291f571439f0a10883d14dfc33cc9eba
jungguen89 <leral3@nate.com>
2023-05-05 02:09:43 +0900

    Initial Commit

---
+++ SwiftUI_Thinking_Bootcamp/SwiftUI_Thinking_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Thinking_BootcampApp.swift
+//  SwiftUI_Thinking_Bootcamp
+//
+//  Created by 권정근 on 2023/05/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Thinking_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
