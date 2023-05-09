7f029bfaed8547e540a8161deaa50613e8ec0a04
jungguen89 <leral3@nate.com>
2023-05-10 01:36:56 +0900

    background와 overlay 사용하는 방법

--- SwiftUI_Thinking_Bootcamp/BackgroundAndOverlayBootcamp.swift
+++ SwiftUI_Thinking_Bootcamp/BackgroundAndOverlayBootcamp.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct BackgroundAndOverlayBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("kick")
+            // resizable() 하고 난 후에 scaledToFit() 할것
+            .resizable()
+            .scaledToFit()
+
+            .frame(width: 470, height: 270)
+            // overlay는 위로 쌓인다.
+            .overlay(
+                Image(systemName: "heart.fill")
+                    .font(.system(size: 70))
+                    .shadow(color: .yellow, radius: 10, x: 0, y: 10)
+                    
+                    // background는 밑으로 쌓인다.
+                    .background(
+                        Image(systemName: "heart.fill")
+                            .font(.system(size: 100))
+                            .foregroundColor(.blue)
+                            .shadow(color: .red, radius: 10, x: 0, y: 10)
+                    )
+                    .offset(x:150, y:100)
+            )
     }
 }
 
