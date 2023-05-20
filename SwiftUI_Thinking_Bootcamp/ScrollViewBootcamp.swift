6a6a729bab5874857ffa5849d21b6c74788d73e9
jungguen89 <leral3@nate.com>
2023-05-20 23:29:04 +0900

    Implementing a ScrollView in SwiftUI

---
+++ SwiftUI_Thinking_Bootcamp/ScrollViewBootcamp.swift
@@ -0,0 +1,37 @@
+//
+//  ScrollViewBootcamp.swift
+//  SwiftUI_Thinking_Bootcamp
+//
+//  Created by 권정근 on 2023/05/20.
+//
+
+import SwiftUI
+
+struct ScrollViewBootcamp: View {
+    var body: some View {
+        
+        
+        ScrollView {
+            VStack {
+                ForEach(0..<10) { index in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(0..<10) { index in
+                                Circle()
+                                    .fill(Color.blue)
+                                    .frame(width: 100, height: 100)
+                                    .shadow(color: .orange, radius: 3, x: 0, y: 0)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ScrollViewBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewBootcamp()
+    }
+}
