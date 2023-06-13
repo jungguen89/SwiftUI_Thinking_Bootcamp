7689f9cf8f8a227338eba26c555a63b9fe5f77f9
jungguen89 <leral3@nate.com>
2023-06-13 19:07:37 +0900

    Create a tab bar with TabView and PageTabViewStyle in SwiftUI

---
+++ SwiftUI_Thinking_Bootcamp/TabViewBootcamp.swift
@@ -0,0 +1,89 @@
+//
+//  TabViewBootcamp.swift
+//  SwiftUI_Thinking_Bootcamp
+//
+//  Created by 권정근 on 2023/06/13.
+//
+
+import SwiftUI
+
+struct TabViewBootcamp: View {
+    
+    @State var selectedTab: Int = 0
+    
+    let icons:[String] = [
+    "heart.fill", "globe", "house.fill", "person.fill"]
+    
+    var body: some View {
+        TabView {
+            ForEach(icons, id:\.self) { icon in
+                Image(systemName: icon)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(30)
+            }
+        }
+        .background(.blue)
+        .shadow(color: .yellow, radius: 20, x: -10, y: 0)
+        .frame(height: 300)
+        .tabViewStyle(PageTabViewStyle())
+        
+        
+//        TabView (selection: $selectedTab){
+//
+//            HomeView(selectedTab: $selectedTab)
+//                .tabItem {
+//                    Image(systemName: "house.fill")
+//                    Text("Home")
+//                }
+//                .tag(0)
+//            Text("Browse Tab")
+//                .tabItem {
+//                    Image(systemName: "globe")
+//                    Text("Browse")
+//                }
+//                .tag(1)
+//            Text("Profile Tab")
+//                .tabItem {
+//                    Image(systemName: "person.fill")
+//                    Text("Profile")
+//                }
+//                .tag(2)
+//        }
+//        .tint(Color.green)
+    }
+}
+
+struct TabViewBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TabViewBootcamp()
+    }
+}
+
+struct HomeView: View {
+    
+    @Binding var selectedTab: Int
+    
+    var body: some View {
+        ZStack {
+            Color.yellow.ignoresSafeArea(.all)
+            VStack {
+                Text("Home Tab")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                
+                Button(action: {
+                    selectedTab = 0
+                }, label: {
+                    Text("Go to profile")
+                        .font(.headline)
+                        .padding()
+                        .padding(.horizontal)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                })
+            }
+        }
+
+    }
+}
